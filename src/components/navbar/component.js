65a39641871438bfce19cd248a1c01e4e7880206
Joe Philip <joe.philip@hotmail.co.in>
2023-03-15 21:25:10 +0530

    added link to work-experience section

--- src/components/navbar/component.js
+++ src/components/navbar/component.js
@@ -26,7 +26,7 @@ function Navbar() {
                                 : ''
                         }
                     </li>
-                    <li className='nav-item'><a href='#' className='nav-link'>Work Experience</a></li>
+                    <li className='nav-item'><a href='#work-experience' className='nav-link'>Work Experience</a></li>
                     <li className='nav-item'><a href='#' className='nav-link'>Projects</a></li>
                     <li className='nav-item'><a href='#' className='nav-link'>Education</a></li>
                     <li className='nav-item'><a href='#' className='nav-link'>About me</a></li>
