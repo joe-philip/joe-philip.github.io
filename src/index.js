626455f40eecaa9da3b72e0523a808d0b0dd909f
Joe Philip <joe.philip@hotmail.co.in>
2023-03-16 12:00:46 +0530

    added projects component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Navbar from './components/navbar/component';
+import ProjectsComponent from './components/projects/component';
 import Experience from './components/workexperience/component';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <Navbar />
     <Experience />
+    <ProjectsComponent />
   </React.StrictMode>
 );
 
