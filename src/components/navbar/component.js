730d2690e28a941e7127b7c87d0238a57b9e263f
Joe Philip <joe.philip@hotmail.co.in>
2023-03-17 13:17:37 +0530

    defined animation for nav-links

--- src/components/navbar/component.js
+++ src/components/navbar/component.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './style.css'
 
 const headerOpenAnimation = [{ height: '21px' }, { height: '137px' }];
+const navLinkAnimation = [{ opacity: 0 }, { opacity: 0 }, { opacity: 1 }];
 const fadeOutAnimation = [{ opacity: 0 }, { opacity: 1 }];
 const headerCloseAnimation = [{ height: '137px' }, { height: '21px' }];
 const animationTiming = { duration: 1000, iterations: 1 };
@@ -10,14 +11,13 @@ function Navbar() {
     const [screenWidth, setScreenWidth] = useState(window.innerWidth)
     const [menuState, setMenuState] = useState('closed')
     window.addEventListener('resize', () => setScreenWidth(window.innerWidth))
+
     return (
         <header id='header'>
             <nav id='navbar'>
                 <ul id='navlist'>
                     <li className='nav-item'>
-                        <a href='/' className='nav-link'>
-                            Joe Philip
-                        </a>
+                        <a href='/' className='nav-link'>Joe Philip</a>
                         {
                             screenWidth <= 412 ?
                                 menuState === 'closed' ?
@@ -29,7 +29,7 @@ function Navbar() {
                     <li className='nav-item'>
                         <a href='#work-experience' className='nav-link'>Work Experience</a>
                     </li>
-                    <li className='nav-item'><a href='/' className='nav-link'>Projects</a></li>
+                    <li className='nav-item'><a href='#projects' className='nav-link'>Projects</a></li>
                     <li className='nav-item'><a href='/' className='nav-link'>Education</a></li>
                     <li className='nav-item'><a href='/' className='nav-link'>About me</a></li>
                 </ul>
@@ -39,7 +39,14 @@ function Navbar() {
     function dropDownOpen(e) {
         e.preventDefault();
         setMenuState('opened')
-        e.target.animate(fadeOutAnimation, animationTiming);
+        var firstElement = true;
+        e.target.animate(fadeOutAnimation, animationTiming); // eslint-disable-next-line
+        [...document.getElementsByClassName('nav-link')].map(i => {
+            if (!firstElement) {
+                i.animate(navLinkAnimation, { ...animationTiming, duration: 2000 });
+            }
+            firstElement = false;
+        });
         var header = document.getElementById('header');
         header.animate(headerOpenAnimation, animationTiming);
         header.style.overflowY = 'initial';
