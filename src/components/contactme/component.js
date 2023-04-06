cc4f67b86f62211cb7aeb95d706fe1344d2c7db7
Joe Philip <joe.philip@hotmail.co.in>
2023-04-06 21:04:49 +0530

    defined contactme form

---
+++ src/components/contactme/component.js
@@ -0,0 +1,28 @@
+import Heading from "../heading/component";
+import './style.css';
+
+function ContactMe() {
+    return (
+        <section id="contact-me">
+            <Heading text="Contact me" />
+            <form id="contact-me-form">
+                <div className="form-row">
+                    <input type="text" placeholder="First name" />
+                    <input type="text" placeholder="Last name" />
+                </div>
+                <div className="form-row">
+                    <input type="email" placeholder="E-mail" />
+                    <input type="text" placeholder="Phone" />
+                </div>
+                <div className="form-row">
+                    <input type="text" placeholder="Subject" />
+                </div>
+                <div className="form-row">
+                    <input type="textarea" placeholder="Message" />
+                </div>
+            </form>
+        </section>
+    );
+}
+
+export default ContactMe;
