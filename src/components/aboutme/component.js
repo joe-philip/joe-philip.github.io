7929e7fd3a1d9ea693859000b3ccce9c8fd7b220
Joe Philip <joe.philip@hotmail.co.in>
2023-10-12 15:11:33 +0530

    moved info to json data

--- src/components/aboutme/component.js
+++ src/components/aboutme/component.js
@@ -1,7 +1,7 @@
 import Heading from '../heading/component';
 import ContactComponent from './contact-component/component';
 import contactData from './contact-component/contact.json';
-import image from './image.jpg';
+import data from './data.json';
 import SkillsList from './skills-component/component';
 import skillsData from './skills-component/data.json';
 import socialsData from './socials-component/data.json';
@@ -16,7 +16,7 @@ function AboutMeComponent() {
                 <div className='about-me-card'>
                     <div className='about-me-section-1'>
                         <div className='about-me-img-container'>
-                            <img src={image} alt='img' className='about-me-img' />
+                            <img src={data.image} alt='img' className='about-me-img' />
                         </div>
                         <ContactComponent data={contactData} />
                         <div className='skills'>
@@ -30,14 +30,7 @@ function AboutMeComponent() {
                     </div>
                     <div className='about-me-section-2'>
                         <h3>About me</h3>
-                        <p className='about-me-intro'>
-                            Hi, I'm Joe Philip and I'm a <b>Python developer</b> with more than 2 years of experience. I'm passionate about writing clean, efficient, and maintainable code that solves real-world problems.
-                            My journey as a developer started when I was 20 years old and was fascinated by how technology can solve complex problems. I learned programming on my own and started working on small projects of my own, which eventually led me to pursue a career as a developer.
-                            Over the years, I've worked on a wide range of projects built using Python's most popular web framework <b>Django</b>. I'm comfortable with back-end development and have experience working with databases like MySQL and PostgreSQL.
-                            One of my favorite things about Python is its simplicity and readability. I strive to write code that is easy to understand, maintain, and scale.
-                            In my free time, I love to contribute to open-source projects and stay up-to-date with the latest developments in the Python community.
-                            If you're looking for a Python developer who is passionate about writing clean, efficient, and maintainable code, then I'd love to work with you. Feel free to get in touch with me to discuss your project requirements.
-                        </p>
+                        <p className='about-me-intro' dangerouslySetInnerHTML={{ __html: data.info }}></p>
                     </div>
                 </div>
             </div>
