d9a15e810d01a547b2e675662a1fe7977f49abf6
Joe Philip <joe.philip@hotmail.co.in>
2023-11-23 11:24:38 +0530

    changed refresh element to button

--- src/components/refresh/component.js
+++ src/components/refresh/component.js
@@ -1,15 +1,20 @@
 import { useDispatch } from 'react-redux';
-import './style.css'
 import { toggleRefreshAction } from '../../redux/actions/toggleTheme';
+import './style.css';
 
 function RefreshPageComponent() {
     const dispatch = useDispatch()
 
     return (
         <div id="refresh">
-            <a href='#' onClick={() => { dispatch(toggleRefreshAction()) }}>
+            <button
+                onClick={
+                    () => {
+                        return dispatch(toggleRefreshAction());
+                    }
+                }>
                 <ion-icon name="reload"></ion-icon>
-            </a>
+            </button>
         </div>
     )
 }
