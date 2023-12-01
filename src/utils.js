fa523f81db0f36a78026d895399fcd5c2da0e2c7
Joe Philip <joe.philip@hotmail.co.in>
2023-12-01 15:49:39 +0530

    added background images for dark theme and light themes

--- src/utils.js
+++ src/utils.js
@@ -2,13 +2,16 @@ const darkThemeColors = {
     'color-background-primary': '#333333',
     'color-background-alternate': '#222222',
     'color-text-primary': '#ffffff',
-    'color-text-secondary': '#cccccc'
+    'color-text-secondary': '#cccccc',
+    'background-image': 'url(dark_bg.jpg)',
+    
 }
 const lightThemeColors = {
     'color-background-primary': '#ffffff',
     'color-background-alternate': '#f5f5f5',
     'color-text-primary': '#333333',
-    'color-text-secondary': '#666666'
+    'color-text-secondary': '#666666',
+    'background-image': 'url(light_bg.jpg)',
 }
 var transparency = 'ff'
 function setDarkTheme() {
@@ -17,6 +20,7 @@ function setDarkTheme() {
     root.style.setProperty('--color-background-alternate', darkThemeColors['color-background-alternate']);
     root.style.setProperty('--color-text-primary', darkThemeColors['color-text-primary']);
     root.style.setProperty('--color-text-secondary', darkThemeColors['color-text-secondary']);
+    root.style.setProperty('--background-img', darkThemeColors['background-image']);
     const navbarBG = getComputedStyle(root).getPropertyValue('--navbar-bg');
     if (navbarBG.length === 9) {
         transparency = navbarBG.slice(7);
@@ -28,6 +32,7 @@ function setLightTheme() {
     root.style.setProperty('--color-background-primary', lightThemeColors['color-background-primary']);
     root.style.setProperty('--color-background-alternate', lightThemeColors['color-background-alternate']);
     root.style.setProperty('--color-text-primary', lightThemeColors['color-text-primary']);
+    root.style.setProperty('--background-img', lightThemeColors['background-image']);
     const navbarBG = getComputedStyle(root).getPropertyValue('--navbar-bg');
     root.style.setProperty('--color-text-secondary', lightThemeColors['color-text-secondary']);
     if (navbarBG.length === 9) {
